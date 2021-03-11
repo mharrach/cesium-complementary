166752181072b37b0044834d6eecc2b8b3e71c9a
mouna harrach <mharrach@gaia3d.com>
2021-03-11 12:02:46 +0900

    added animation

--- tracking_vehicle.js
+++ tracking_vehicle.js
@@ -2,9 +2,11 @@
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNDkwZjJhOS1mMzk1LTQyNWQtODUxNS05MzIzMjczYjJiM2IiLCJpZCI6MzE4OTIsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTYxMDMxNDZ9.VNubg4IInTepymHyJauFNquxJAUQ36wgFHttjWL7aec";
 
 var viewer = new Cesium.Viewer("cesiumContainer", {
-    terrainProvider: new Cesium.CesiumTerrainProvider({
+    shouldAnimate: true, // Enable animations
+    /*terrainProvider: new Cesium.CesiumTerrainProvider({
         url: Cesium.IonResource.fromAssetId(1),
-    }),
+    }),*/
+    terrainProvider: Cesium.createWorldTerrain(),
 });
 viewer.scene.globe.depthTestAgainstTerrain = true;
 
@@ -35,11 +37,16 @@ tileset.readyPromise.then(function() {
     console.log(error);
 });
 
+/*if (scene.clampToHeightSupported) {
+    tileset.initialTilesLoaded.addEventListener(start);
+} else {
+    window.alert("This browser does not support clampToHeight.");
+}*/
+
 // setting up the path and model
 var scene = viewer.scene;
 var clock = viewer.clock;
 
-var entity;
 var positionProperty = [];
 
 var dataSourcePromise = Cesium.GeoJsonDataSource.load('Irisbus_path.geojson', {
@@ -55,36 +62,6 @@ dataSourcePromise.then(function(dataSource) {
 }).otherwise(function(error) {
     window.alert(error);
 });
-
-function createModel(url, height) {
-
-    var position = Cesium.Cartesian3.fromDegrees(126.8882616,
-        37.5797226,
-        height
-    );
-    var heading = Cesium.Math.toRadians(220);
-    var pitch = 0;
-    var roll = 0;
-    var hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
-    var orientation = Cesium.Transforms.headingPitchRollQuaternion(
-        position,
-        hpr
-    );
-
-    entity = viewer.entities.add({
-        name: url,
-        position: position,
-        orientation: orientation,
-        model: {
-            uri: url,
-            minimumPixelSize: 80,
-            maximumScale: 100,
-        },
-    });
-    //viewer.trackedEntity = entity;
-}
-
-createModel("SampleData/models/GroundVehicle/GroundVehicle.glb", 3.5);
 
 var xmlhttp = new XMLHttpRequest();
 var url = "Irisbus_path.geojson";
@@ -102,25 +79,82 @@ function myFunction(geojson) {
     var coordinates = geojson.features[0].geometry.coordinates[0];
     for (let i = 0; i < coordinates.length; i++) {
         const element = coordinates[i];
-        positionProperty.push(Cesium.Cartesian3.fromDegrees(element[0], element[1], 0));
+        positionProperty.push(element[0], element[1], 0);
     }
-}
+    //Compute the entity position property.
+    var position = computeBusPath(positionProperty);
 
-if (scene.clampToHeightSupported) {
-    tileset.initialTilesLoaded.addEventListener(start);
-} else {
-    window.alert("This browser does not support clampToHeight.");
-}
+    //Actually create the entity
+    var entity = viewer.entities.add({
+        //Set the entity availability to the same interval as the simulation time.
+        availability: new Cesium.TimeIntervalCollection([
+            new Cesium.TimeInterval({
+                start: start,
+                stop: stop,
+            }),
+        ]),
 
-function start() {
-    clock.shouldAnimate = true;
-    var objectsToExclude = [entity];
-    scene.postRender.addEventListener(function() {
-        /*for (let j = 0; j < positionProperty.length; j++) {
-            const position = positionProperty[j];
-            entity.position = scene.clampToHeight(position, objectsToExclude);
-        }*/
-        //var position = positionProperty.getValue(clock.currentTime);
-        //entity.position = scene.clampToHeight(position, objectsToExclude);
+        //Use our computed positions
+        position: position,
+
+        //Automatically compute orientation based on position movement.
+        orientation: new Cesium.VelocityOrientationProperty(position),
+
+        //Load the Cesium plane model to represent the entity
+        model: {
+            uri: "SampleData/models/CesiumMilkTruck/CesiumMilkTruck.glb",
+            minimumPixelSize: 50,
+            maximumPixelSize: 80
+        },
+
+        //Show the path as a pink line sampled in 1 second increments.
+        /*path: {
+            resolution: 1,
+            material: new Cesium.PolylineGlowMaterialProperty({
+                glowPower: 0.1,
+                color: Cesium.Color.YELLOW,
+            }),
+            width: 10,
+        },
+
+        clampToGround: true*/
     });
+
+}
+
+//Set bounds of our simulation time
+var start = Cesium.JulianDate.fromDate(new Date(2015, 2, 25, 16));
+var stop = Cesium.JulianDate.addSeconds(
+    start,
+    560,
+    new Cesium.JulianDate()
+);
+
+//Make sure viewer is at the desired time.
+viewer.clock.startTime = start.clone();
+viewer.clock.stopTime = stop.clone();
+viewer.clock.currentTime = start.clone();
+viewer.clock.clockRange = Cesium.ClockRange.LOOP_STOP; //Loop at the end
+viewer.clock.multiplier = 10;
+
+//Set timeline to simulation bounds
+viewer.timeline.zoomTo(start, stop);
+
+//Generate a random circular pattern with varying heights.
+function computeBusPath(posList) {
+    var property = new Cesium.SampledPositionProperty();
+    for (var i = 0; i <= posList.length - 3; i += 3) {
+        var time = Cesium.JulianDate.addSeconds(
+            start,
+            i + 45,
+            new Cesium.JulianDate()
+        );
+        var position = Cesium.Cartesian3.fromDegrees(
+            posList[i],
+            posList[i + 1],
+            posList[i + 2] + 50
+        );
+        property.addSample(time, position);
+    }
+    return property;
 }
